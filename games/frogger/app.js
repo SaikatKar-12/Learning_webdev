dd8e0fcf7279d52d42fefdac03959da148464e86
Saikat Kar <ksaikat009@gmail.com>
2024-01-14 22:05:30 +0530

    week4

---
+++ games/frogger/app.js
@@ -0,0 +1,6 @@
+const timeleftDisplay=document.querySelector("#time-left")
+const resulDisplay= document.querySelector('#result')
+const startpausebutton=document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+let currentIndex = 76
